afb5b22c5f3044036860f0f6341df1d9951f0b1b
Artem Ivanko <ivankoartem@gmail.com>
2017-07-20 23:11:22 +0300

    Оптимизация размера базы данных +Изменение формата ячеек для таблицы со статистикой по виртуальным серверам. +Изменение размера блока ключа для таблицы со статистикой по виртуальным. +Изменение размера блока ключа для таблицы со снапшотами виртуальных серверов. Выиграш по размеру базы данных: для таблицы со статистикой по виртуальным в 2 раза (данные примерные) для таблицы со снапшотами виртуальных серверов в 11 (данные примерные)

---
+++ database/migrations/2017_07_20_230325_change_key_block_size_statistics_virtual_servers_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeKeyBlockSizeStatisticsVirtualServersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('ALTER TABLE statistics_virtual_servers  KEY_BLOCK_SIZE = 1 , ROW_FORMAT = COMPRESSED');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('ALTER TABLE statistics_virtual_servers ROW_FORMAT=COMPACT;');
+    }
+}
