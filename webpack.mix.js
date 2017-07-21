196c253b45f7e196140469928a73d74a1c0bbda0
Artem Ivanko <ivankoartem@gmail.com>
2017-07-21 23:39:27 +0300

    Подготовка к альфе#4

--- webpack.mix.js
+++ webpack.mix.js
@@ -22,11 +22,7 @@ mix.options({
 });
 
 mix.js('resources/assets/js/app.js', 'public/js')
-
 .sass('resources/assets/sass/app.scss', 'public/css')
-mix.copy('resources/assets/fonts/', 'public/fonts')
-mix.copy('resources/assets/css/', 'public/css')
-mix.copy('resources/assets/img/', 'public/img')
 mix.less('app.less')
 .version();
 
