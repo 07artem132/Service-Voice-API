cd7cd533413b31007f091cef08762ded9f9674be
Artem Ivanko <ivankoartem@gmail.com>
2017-07-23 18:36:53 +0300

    Добавлено логирования сообщение об успешном выполнеии задачи в базу данных.

--- app/Listeners/LogCronJobSuccess.php
+++ app/Listeners/LogCronJobSuccess.php
@@ -2,6 +2,8 @@
 
 namespace Api\Listeners;
 
+use Api\Task;
+use Api\TaskLog;
 use Api\Events\CronJobSuccess;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -21,11 +23,24 @@ class LogCronJobSuccess
     /**
      * Handle the event.
      *
-     * @param  CronJobSuccess  $event
+     * @param  CronJobSuccess $event
      * @return void
      */
     public function handle(CronJobSuccess $event)
     {
-        //
+
+        $task_logs = new TaskLog;
+        $task_logs->task_id = $event->task_id;
+        $task_logs->status = 1;
+        $task_logs->message = 'Задание выполненно успешно';
+        $task_logs->run_time = $event->runtime;
+        $task_logs->save();
+
+        $TaskStatus = task::find($event->task_id);
+
+        $TaskStatus->last_run = date('Y-m-d H:i:s', $event->rundate);
+
+        $TaskStatus->save();
+
     }
 }
