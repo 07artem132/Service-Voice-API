32dc27dafae647cb039942155b0097529b288222
Artem Ivanko <ivankoartem@gmail.com>
2017-07-25 00:41:11 +0300

    Добавлены правила валидации входных данных для параметров создания виртуального сервера

--- config/ApiValidation.php
+++ config/ApiValidation.php
@@ -157,6 +157,173 @@ return [
             ]
 
         ]
-    ]
+    ],
+    'TeamSpeak' => [
+        'VirtualServer' => [
+            'Create' => [
+                'rules' => [
+                    'virtualserver_name' => [
+                        'required',
+                        'string',
+                        'max:64',
+                    ],
+                    'virtualserver_welcomemessage' => [
+                        'string'
+                    ],
+                    'virtualserver_maxclients' => [
+                        'required',
+                        'integer'
+                    ],
+                    'virtualserver_password' => [
+                        'string'
 
+                    ],
+                    'virtualserver_hostmessage' => [
+                        'string'
+
+                    ],
+                    'virtualserver_hostmessage_mode' => [
+                        'integer',
+                        'in:1,2,3'
+                    ],
+                    'virtualserver_default_server_group' => [
+                        'integer'
+
+                    ],
+                    'virtualserver_default_channel_group' => [
+                        'integer'
+
+                    ],
+                    'virtualserver_default_channel_admin_group' => [
+                        'integer'
+
+                    ],
+                    'virtualserver_max_download_total_bandwidth' => [
+                        'integer'
+
+                    ],
+                    'virtualserver_max_upload_total_bandwidth' => [
+                        'integer'
+
+                    ],
+                    'virtualserver_hostbanner_url' => [
+                        'string'
+
+                    ],
+                    'virtualserver_hostbanner_gfx_url' => [
+                        'string'
+
+                    ],
+                    'virtualserver_hostbanner_gfx_interval' => [
+                        'integer'
+
+                    ],
+                    'virtualserver_complain_autoban_count' => [
+                        'integer'
+
+                    ],
+                    'virtualserver_complain_autoban_time' => [
+                        'integer'
+
+                    ],
+                    'virtualserver_complain_remove_time' => [
+                        'integer'
+
+                    ],
+                    'virtualserver_min_clients_in_channel_before_forced_silence' => [
+                        'integer'
+
+                    ],
+                    'virtualserver_priority_speaker_dimm_modificator' => [
+                        'integer'
+
+                    ],
+                    'virtualserver_antiflood_points_tick_reduce' => [
+                        'integer'
+
+                    ],
+                    'virtualserver_antiflood_points_needed_command_block' => [
+                        'integer'
+
+                    ],
+                    'virtualserver_antiflood_points_needed_ip_block' => [
+                        'integer'
+
+                    ],
+                    'virtualserver_hostbanner_mode' => [
+                        'integer'
+
+                    ],
+                    'virtualserver_hostbutton_tooltip' => [
+                        'string'
+
+                    ],
+                    'virtualserver_hostbutton_gfx_url' => [
+                        'string'
+
+                    ],
+                    'virtualserver_hostbutton_url' => [
+                        'string'
+                    ],
+                    'virtualserver_download_quota' => [
+                        'integer'
+                    ],
+                    'virtualserver_upload_quota' => [
+                        'integer'
+                    ],
+                    'virtualserver_machine_id' => [
+                        'integer'
+                    ],
+                    'virtualserver_port' => [
+                        'integer'
+                    ],
+                    'virtualserver_autostart' => [
+                        'integer'
+                    ],
+                    'virtualserver_log_client' => [
+                        'integer'
+                    ],
+                    'virtualserver_log_query' => [
+                        'integer'
+                    ],
+                    'virtualserver_log_channel' => [
+                        'integer'
+                    ],
+                    'virtualserver_log_permissions' => [
+                        'integer'
+                    ],
+                    'virtualserver_log_server' => [
+                        'integer'
+                    ],
+                    'virtualserver_log_filetransfer' => [
+                        'integer'
+                    ],
+                    'virtualserver_min_client_version' => [
+                        'integer'
+                    ],
+                    'virtualserver_needed_identity_security_level' => [
+                        'integer'
+                    ],
+                    'virtualserver_name_phonetic' => [
+                        'string'
+                    ],
+                    'virtualserver_icon_id' => [
+                        'integer'
+                    ],
+                    'virtualserver_reserved_slots' => [
+                        'integer'
+                    ],
+                    'virtualserver_weblist_enabled' => [
+                        'integer'
+                    ],
+                    'virtualserver_codec_encryption_mode' => [
+                        'integer'
+                    ],
+                ],
+                'messages' => [
+                    'max' => 'Максимальное значение поля :attribute составляет :max (int)',
+                ],
+            ],
+        ],
+    ],
 ];
