ac426a251b2768b4bed707c520b87259b3063695
Artem Ivanko <ivankoartem@gmail.com>
2017-07-29 18:33:14 +0300

    Добавлено исключение Вам не разрешено удалять данный токен

---
+++ app/Exceptions/TokenNotAllowedDeleteException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Artem
+ * Date: 29.07.2017
+ * Time: 10:25
+ */
+
+namespace Api\Exceptions;
+
+use Exception;
+
+class TokenNotAllowedDeleteException extends Exception
+{
+    public function __construct()
+    {
+        //
+    }
+
+}
