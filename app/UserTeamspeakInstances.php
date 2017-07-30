1c32c223c5d4831299bafe96acbbb4e8d4aabf9b
Artem Ivanko <ivankoartem@gmail.com>
2017-07-30 18:29:20 +0300

    Рефакторинг (изменение названия модели) С UserServer на UserTeamspeakInstances

---
+++ app/UserTeamspeakInstances.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Api;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Api\UserTeamspeakInstances
+ *
+ * @property int $id
+ * @property string $user_id
+ * @property string $server_id
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\Api\UserTeamspeakInstances whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Api\UserTeamspeakInstances whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Api\UserTeamspeakInstances whereServerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Api\UserTeamspeakInstances whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Api\UserTeamspeakInstances whereUserId($value)
+ * @mixin \Eloquent
+ */
+class UserTeamspeakInstances extends Model
+{
+    //
+}
