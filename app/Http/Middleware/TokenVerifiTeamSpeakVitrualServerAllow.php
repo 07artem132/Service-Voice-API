3331f7ed50dc73b204a48c71bd179f7344a8b921
Artem Ivanko <ivankoartem@gmail.com>
2017-07-30 22:31:35 +0300

    Добавление нового защитника Выполняется проверка разрешено ли данному токену взаимодействовать с запрашиваемым виртуальным сервером

---
+++ app/Http/Middleware/TokenVerifiTeamSpeakVitrualServerAllow.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Artem
+ * Date: 30.07.2017
+ * Time: 12:07
+ */
+
+namespace Api\Http\Middleware;
+
+use Api\UserToken;
+use Closure;
+
+class TokenVerifiTeamSpeakVitrualServerAllow
+{
+
+    public function handle($request, Closure $next)
+    {
+
+        $server_id = (int)$request->server_id;
+        $uid = base64_decode($request->bashe64uid);
+        $token = $request->header('X-token');
+
+        $result = UserToken::with('servers', 'TeamspeakVirtualServers')->where('token', '=', $token)->first();
+
+        $AllowServers = $result->servers->toArray();
+        $AllowTeamspeakVirtualServers = $result->TeamspeakVirtualServers->toArray();
+
+
+        if ($this->VerifiAllowServers($server_id, $AllowServers)
+            || $this->VerifiAllowTeamspeakVirtualServers($server_id, $uid, $AllowTeamspeakVirtualServers))
+            return $next($request);
+
+        throw new TeamspeakVirtualServersNotAllowExceptions($server_id, $uid);
+    }
+
+    function VerifiAllowServers(int $server_id, ?array $AllowServers): bool
+    {
+        if (empty($AllowServers))
+            return false;
+
+        for ($i = 0; $i < count($AllowServers); $i++)
+            if ($server_id === $AllowServers[$i]['instance_id'])
+                return true;
+
+        return false;
+    }
+
+    function VerifiAllowTeamspeakVirtualServers(int $server_id, string $uid, ?array $AllowTeamspeakVirtualServers): bool
+    {
+        if (empty($AllowTeamspeakVirtualServers))
+            return false;
+
+        for ($i = 0; $i < count($AllowTeamspeakVirtualServers); $i++)
+            if ($server_id === $AllowTeamspeakVirtualServers[$i]['instance_id'] && $uid === $AllowTeamspeakVirtualServers[$i]['unique_id'])
+                return true;
+
+        return false;
+    }
+}
