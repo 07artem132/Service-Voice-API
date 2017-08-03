a4974827c8b58ddbaf8204dfec7935773f310dda
Artem Ivanko <ivankoartem@gmail.com>
2017-08-03 15:14:30 +0300

    Добавление правил валидации и собщений об ошибке валидации Каналы: создание,редактирование,перемещение

--- config/ApiValidation.php
+++ config/ApiValidation.php
@@ -412,6 +412,110 @@ return [
 
                 ]
             ],
+            'channel' => [
+                'create' => [
+                    'rules' => [
+                        'cpid' => [
+                            'integer',
+                        ],
+                        'channel_flag_permanent' => [
+                            'boolean',
+                        ],
+                        'channel_name' => [
+                            'required',
+                            'string',
+                        ],
+                        '*' => [
+                            'in_array:' . implode(',', [
+                                "channel_name",
+                                "channel_topic",
+                                "channel_description",
+                                "channel_password",
+                                "channel_codec",
+                                "channel_codec_quality",
+                                "channel_maxclients",
+                                "channel_maxfamilyclients",
+                                "channel_order",
+                                "channel_flag_permanent",
+                                "channel_flag_semi_permanent",
+                                "channel_flag_temporary",
+                                "channel_flag_default",
+                                "channel_flag_maxclients_unlimited",
+                                "channel_flag_maxfamilyclients_unlimited",
+                                "channel_flag_maxfamilyclients_inherited",
+                                "channel_needed_talk_power",
+                                "channel_name_phonetic",
+                                "channel_icon_id",
+                                "channel_codec_is_unencrypted",
+                                "cpid",
+                            ]),
+                        ]
+                    ],
+                    'messages' => [
+                        'string' => 'Ожидалось что поле :attribute будет строкой (string)',
+                        '*.in_array' => 'Не ожидалось что будет передано поле :attribute, просьба проверить документацию.',
+                        'integer' => 'Ожидалось что в поле :attribute будет передано число (int)',
+                    ],
+                ],
+                'edit' => [
+                    'rules' => [
+                        'cpid' => [
+                            'integer',
+                        ],
+                        'channel_flag_permanent' => [
+                            'boolean',
+                        ],
+                        'channel_name' => [
+                            'string',
+                        ]
+         /*               '*' => [
+                            'in_array:' . implode(',', [
+                                "channel_name",
+                                "channel_topic",
+                                "channel_description",
+                                "channel_password",
+                                "channel_codec",
+                                "channel_codec_quality",
+                                "channel_maxclients",
+                                "channel_maxfamilyclients",
+                                "channel_order",
+                                "channel_flag_permanent",
+                                "channel_flag_semi_permanent",
+                                "channel_flag_temporary",
+                                "channel_flag_default",
+                                "channel_flag_maxclients_unlimited",
+                                "channel_flag_maxfamilyclients_unlimited",
+                                "channel_flag_maxfamilyclients_inherited",
+                                "channel_needed_talk_power",
+                                "channel_name_phonetic",
+                                "channel_icon_id",
+                                "channel_codec_is_unencrypted",
+                                "cpid",
+                            ]),
+                          ]*/
+                    ],
+                    'messages' => [
+                        'string' => 'Ожидалось что поле :attribute будет строкой (string)',
+                        'integer' => 'Ожидалось что в поле :attribute будет передано число (int)',
+                    ],
+                ],
+                'move' => [
+                    'rules' => [
+                        'channel_parent_id' => [
+                            'required',
+                            'integer',
+                        ],
+                        'channel_sort_order' => [
+                            'required',
+                            'integer',
+                        ]
+                    ],
+                    'messages' => [
+                        'string' => 'Ожидалось что поле :attribute будет строкой (string)',
+                        'integer' => 'Ожидалось что в поле :attribute будет передано число (int)',
+                    ],
+                ]
+            ],
         ],
     ]
 ];
