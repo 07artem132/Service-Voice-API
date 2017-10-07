547f147ee77fade2d07fcbbb0c15078d0a2a4843
Artem Ivanko <ivankoartem@gmail.com>
2017-10-07 18:21:31 +0300

    Добавление правил валидации

--- config/ApiValidation.php
+++ config/ApiValidation.php
@@ -468,31 +468,31 @@ return [
                         'channel_name' => [
                             'string',
                         ]
-         /*               '*' => [
-                            'in_array:' . implode(',', [
-                                "channel_name",
-                                "channel_topic",
-                                "channel_description",
-                                "channel_password",
-                                "channel_codec",
-                                "channel_codec_quality",
-                                "channel_maxclients",
-                                "channel_maxfamilyclients",
-                                "channel_order",
-                                "channel_flag_permanent",
-                                "channel_flag_semi_permanent",
-                                "channel_flag_temporary",
-                                "channel_flag_default",
-                                "channel_flag_maxclients_unlimited",
-                                "channel_flag_maxfamilyclients_unlimited",
-                                "channel_flag_maxfamilyclients_inherited",
-                                "channel_needed_talk_power",
-                                "channel_name_phonetic",
-                                "channel_icon_id",
-                                "channel_codec_is_unencrypted",
-                                "cpid",
-                            ]),
-                          ]*/
+                        /*               '*' => [
+                                           'in_array:' . implode(',', [
+                                               "channel_name",
+                                               "channel_topic",
+                                               "channel_description",
+                                               "channel_password",
+                                               "channel_codec",
+                                               "channel_codec_quality",
+                                               "channel_maxclients",
+                                               "channel_maxfamilyclients",
+                                               "channel_order",
+                                               "channel_flag_permanent",
+                                               "channel_flag_semi_permanent",
+                                               "channel_flag_temporary",
+                                               "channel_flag_default",
+                                               "channel_flag_maxclients_unlimited",
+                                               "channel_flag_maxfamilyclients_unlimited",
+                                               "channel_flag_maxfamilyclients_inherited",
+                                               "channel_needed_talk_power",
+                                               "channel_name_phonetic",
+                                               "channel_icon_id",
+                                               "channel_codec_is_unencrypted",
+                                               "cpid",
+                                           ]),
+                                         ]*/
                     ],
                     'messages' => [
                         'string' => 'Ожидалось что поле :attribute будет строкой (string)',
@@ -508,6 +508,24 @@ return [
                         'channel_sort_order' => [
                             'required',
                             'integer',
+                        ]
+                    ],
+                    'messages' => [
+                        'string' => 'Ожидалось что поле :attribute будет строкой (string)',
+                        'integer' => 'Ожидалось что в поле :attribute будет передано число (int)',
+                    ],
+                ],
+
+            ],
+            'Token' => [
+                'Create' => [
+                    'rules' => [
+                        'groupID' => [
+                            'required',
+                            'integer',
+                        ],
+                        'description' => [
+                            'string',
                         ]
                     ],
                     'messages' => [
