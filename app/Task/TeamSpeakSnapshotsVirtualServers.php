d57c06b52c49ba7ef495614858441fe41f5f5ee5
Artem Ivanko <ivankoartem@gmail.com>
2017-10-09 20:25:05 +0300

    Изменения в логике работы Для виртуальных серверов со статусом отличным от online снапшоты не создаются. Так же в случае если на инстансе не было подходящих серверов цикл foreach для сохранения данных в базу данных не исполняется.

--- app/Task/TeamSpeakSnapshotsVirtualServers.php
+++ app/Task/TeamSpeakSnapshotsVirtualServers.php
@@ -39,14 +39,16 @@ class TeamSpeakSnapshotsVirtualServers
     {
         $this->ts3con = new TeamSpeak($instance_id);
         $data = $this->GetAllServersSnapshots();
-        foreach ($data as $VirtualServerSnapshots) {
-            $db = new SnapshotsTeamspeakVirtualServers;
-            $db->instance_id = $instance_id;
-            $db->unique_id = $VirtualServerSnapshots['unique_id'];
-            $db->port = $VirtualServerSnapshots['port'];
-            $db->snapshot = $VirtualServerSnapshots['snapshot'];
-            $db->save();
-        }
+
+        if (!empty($data))
+            foreach ($data as $VirtualServerSnapshots) {
+                $db = new SnapshotsTeamspeakVirtualServers;
+                $db->instance_id = $instance_id;
+                $db->unique_id = $VirtualServerSnapshots['unique_id'];
+                $db->port = $VirtualServerSnapshots['port'];
+                $db->snapshot = $VirtualServerSnapshots['snapshot'];
+                $db->save();
+            }
 
         $this->ts3con->logout();
 
@@ -56,10 +58,12 @@ class TeamSpeakSnapshotsVirtualServers
     /**
      * @return array набор данных подготовленных для сохранения в базу данных.
      */
-    function GetAllServersSnapshots(): array
+    function GetAllServersSnapshots(): ?array
     {
         foreach ($this->ts3con->ReturnConnection() as $VirtualServer) {
-            $data[$VirtualServer['virtualserver_id']]['port'] = (int)$VirtualServer['virtualserver_port'];
+            if ((string)$VirtualServer['virtualserver_status'] != 'online')
+                continue;
+
             $data[$VirtualServer['virtualserver_id']]['unique_id'] = (string)$VirtualServer['virtualserver_unique_identifier'];
             $data[$VirtualServer['virtualserver_id']]['snapshot'] = (string)$VirtualServer->snapshotCreate();
         }
