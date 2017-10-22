b859cdc2b1bd9a1e6b4c135fc8a67e003920a384
Artem Ivanko <ivankoartem@gmail.com>
2017-10-22 17:45:54 +0300

    Добавление нового функционала для взаимодействия с WHMCS API +Возможность выбора типа аутентификации (старый/новый вариант) +Получение информации о клиенте +Получение списка существующих групп клиентов

--- app/Services/WHMCS/API.php
+++ app/Services/WHMCS/API.php
@@ -8,45 +8,106 @@
 
 namespace Api\Services\WHMCS;
 
-use Api\Exceptions\BillingValidateLoginException;
+use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Client as HTTPClient;
+use Api\Exceptions\BillingIsNotAvailable;
+use Api\Exceptions\BillingValidateLoginException;
+use GuzzleHttp\Exception\RequestException;
 
-class API
-{
-    private $identifier = '';
-    private $secret = '';
-    private $APIURL = '';
+class API {
+	private $request;
+	private $params;
 
-    function __construct()
-    {
+	public function __construct() {
+		$this->request = new HTTPClient();
+	}
 
-        $this->identifier = env('BILLING_ID');
-        $this->secret = env('BILLING_SECRET');
-        $this->APIURL = env('BILLING_API_URL');
-    }
+	private function GetIdentifier(): string {
+		return env( 'BILLING_ID' );
+	}
 
-    function ValidateCredentials($credentials)
-    {
-        $client = new HTTPClient();
+	private function GetUsername(): string {
+		return env( 'BILLING_USERNAME' );
+	}
 
-        $response = $client->request('POST', $this->APIURL, [
-            'form_params' => [
-                'identifier' => $this->identifier,
-                'secret' => $this->secret,
-                'action' => 'ValidateLogin',
-                'email' => $credentials['email'],
-                'password2' => $credentials['password'],
-                'responsetype' => 'json',
+	private function GetSecret(): string {
+		return env( 'BILLING_SECRET' );
+	}
 
-            ]
-        ]);
+	private function GetPassword(): string {
+		return env( 'BILLING_PASSWORD' );
+	}
 
-        $response = json_decode($response->getBody());
+	private function GetApiUrl(): string {
+		return env( 'BILLING_API_URL' );
+	}
 
-        if ($response->result != 'success') {
-            throw new  BillingValidateLoginException($response->message);
-        }
+	private function ParamsAdd( string $key, $value ): void {
+		$this->params['form_params'][ $key ] = $value;
+	}
 
-        return $response;
-    }
+	private function GetParams(): array {
+		return $this->params;
+	}
+
+	private function SetAction( string $Action ): void {
+		$this->ParamsAdd( 'action', $Action );
+	}
+
+	private function send(): Response {
+		if ( env( 'BILLING_7_2_AND_ABOVE', false ) ) {
+			$this->ParamsAdd( 'identifier', $this->GetIdentifier() );
+			$this->ParamsAdd( 'secret', $this->GetSecret() );
+		} else {
+			$this->ParamsAdd( 'username', $this->GetUsername() );
+			$this->ParamsAdd( 'password', $this->GetPassword() );
+		}
+
+		$this->ParamsAdd( 'responsetype', 'json' );
+
+		try {
+			$response = $this->request->post( $this->GetApiUrl(), $this->GetParams() );
+		} catch ( RequestException $e ) {
+			$data = json_decode( $e->getResponse()->getBody()->getContents() );
+			throw  new BillingIsNotAvailable( $data->message );
+			//TODO Не показывать пользователю ошибку, а создать событие и залогировать
+		}
+
+		return $response;
+	}
+
+	function ValidateCredentials( string $email, string $password ): \stdClass {
+		$this->SetAction( 'ValidateLogin' );
+		$this->ParamsAdd( 'email', $email );
+		$this->ParamsAdd( 'password2', $password );
+
+		$response = $this->send();
+		$response = json_decode( $response->getBody() );
+
+		if ( $response->result != 'success' ) {
+			throw new  BillingValidateLoginException( $response->message );
+		}
+
+		return $response;
+	}
+
+	function GetClientsDetails( int $clientid, bool $stats ): \stdClass {
+		$this->SetAction( 'GetClientsDetails' );
+		$this->ParamsAdd( 'clientid', $clientid );
+		$this->ParamsAdd( 'stats', $stats );
+
+		$response = $this->send();
+		$response = json_decode( $response->getBody() );
+
+		return $response;
+	}
+
+	function GetClientGroups(): \stdClass {
+		$this->SetAction( 'GetClientGroups' );
+
+		$response = $this->send();
+		$response = json_decode( $response->getBody() );
+
+		return $response;
+	}
 }
