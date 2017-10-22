54cf830de0a8ece7f86787442ba36fd8a1386296
Artem Ivanko <ivankoartem@gmail.com>
2017-10-22 17:44:14 +0300

    Добавление нового функционала +Удаление снапшотов из базы данных которые существуют более X дней.

--- app/Task/TeamSpeakSnapshotsVirtualServers.php
+++ app/Task/TeamSpeakSnapshotsVirtualServers.php
@@ -16,58 +16,70 @@ use Api\SnapshotsTeamspeakVirtualServers;
  * Class TeamSpeakSnapshotsVirtualServers
  * @package Api\Task
  */
-class TeamSpeakSnapshotsVirtualServers
-{
-    /**
-     * @var teamSpeak класс для взаимодействия с teamspeak 3
-     */
-    private $ts3con;
+class TeamSpeakSnapshotsVirtualServers {
+	/**
+	 * @var teamSpeak класс для взаимодействия с teamspeak 3
+	 */
+	private $ts3con;
+	private $KeepDays;
+	private $CreateDisabledServers;
 
-    function CronCallback()
-    {
-        $servers = TeamspeakInstances::Active()->get();
+	public function __construct() {
+		$this->KeepDays              = config( 'TeamSpeakSnapshotsVirtualServers.KeepDays' );
+		$this->CreateDisabledServers = config( 'TeamSpeakSnapshotsVirtualServers.CreateSnapshotsDisabledVirtualServers' );
+	}
 
-        foreach ($servers as $server) {
-            $this->CreateSnapshots($server->id);
-        }
-    }
+	function CronCallback() {
+		$servers = TeamspeakInstances::Active()->get();
 
-    /**
-     * @param int $instance_id уникальный идентификатор teamspeak 3 инстанса
-     */
-    public function CreateSnapshots(int $instance_id): void
-    {
-        $this->ts3con = new TeamSpeak($instance_id);
-        $data = $this->GetAllServersSnapshots();
+		foreach ( $servers as $server ) {
+			$this->CreateSnapshots( $server->id );
+		}
 
-        if (!empty($data))
-            foreach ($data as $VirtualServerSnapshots) {
-                $db = new SnapshotsTeamspeakVirtualServers;
-                $db->instance_id = $instance_id;
-                $db->unique_id = $VirtualServerSnapshots['unique_id'];
-                $db->snapshot = $VirtualServerSnapshots['snapshot'];
-                $db->save();
-            }
+		$this->RemoveOldSnapshots();
+	}
 
-        $this->ts3con->logout();
+	/**
+	 * @param int $instance_id уникальный идентификатор teamspeak 3 инстанса
+	 */
+	public function CreateSnapshots( int $instance_id ): void {
+		$this->ts3con = new TeamSpeak( $instance_id );
+		$data         = $this->GetAllServersSnapshots();
 
-        return;
-    }
+		if ( ! empty( $data ) ) {
+			foreach ( $data as $VirtualServerSnapshots ) {
+				$db              = new SnapshotsTeamspeakVirtualServers;
+				$db->instance_id = $instance_id;
+				$db->unique_id   = $VirtualServerSnapshots['unique_id'];
+				$db->snapshot    = $VirtualServerSnapshots['snapshot'];
+				$db->save();
+			}
+		}
 
-    /**
-     * @return array набор данных подготовленных для сохранения в базу данных.
-     */
-    function GetAllServersSnapshots(): ?array
-    {
-        foreach ($this->ts3con->ReturnConnection() as $VirtualServer) {
-            if ((string)$VirtualServer['virtualserver_status'] != 'online')
-                continue;
+		$this->ts3con->logout();
 
-            $data[$VirtualServer['virtualserver_id']]['unique_id'] = (string)$VirtualServer['virtualserver_unique_identifier'];
-            $data[$VirtualServer['virtualserver_id']]['snapshot'] = (string)$VirtualServer->snapshotCreate();
-        }
+		return;
+	}
 
-        return $data;
-    }
+	/**
+	 * @return array набор данных подготовленных для сохранения в базу данных.
+	 */
+	function GetAllServersSnapshots(): ?array {
+		foreach ( $this->ts3con->ReturnConnection() as $VirtualServer ) {
+			if ( (string) $VirtualServer['virtualserver_status'] != 'online' && ! $this->CreateDisabledServers ) {
+				continue;
+			}
 
+			$data[ $VirtualServer['virtualserver_id'] ]['unique_id'] = (string) $VirtualServer['virtualserver_unique_identifier'];
+			$data[ $VirtualServer['virtualserver_id'] ]['snapshot']  = (string) $VirtualServer->snapshotCreate();
+		}
+
+		return $data;
+	}
+
+	function RemoveOldSnapshots(): void {
+		SnapshotsTeamspeakVirtualServers::GetOlder( $this->KeepDays )->delete();
+
+		return;
+	}
 }
