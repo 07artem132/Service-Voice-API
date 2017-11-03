3d7abc9aa08ec20a7b3822d72046360eddf3be66
Artem Ivanko <ivankoartem@gmail.com>
2017-11-03 17:39:52 +0200

    Исправление багов

--- app/Providers/CronServiceProvider.php
+++ app/Providers/CronServiceProvider.php
@@ -6,29 +6,33 @@ use Api\Task;
 use \Liebig\Cron\Cron;
 use Illuminate\Support\ServiceProvider;
 
-class CronServiceProvider extends ServiceProvider
-{
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $Tasks = Task::Active()->orderBy('priority')->get();
+class CronServiceProvider extends ServiceProvider {
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+		$Tasks = Task::Active()->orderBy( 'priority' )->get();
 
-        foreach ($Tasks as $Task) {
-            Cron::add($Task->name, $Task->frequency, [$c = new $Task->class_name, 'CronCallback'], (bool)$Task->is_enabled);
-        }
-    }
+		try {
+			foreach ( $Tasks as $Task ) {
+				Cron::add( $Task->name, $Task->frequency, [
+					$c = new $Task->class_name,
+					'CronCallback'
+				], (bool) $Task->is_enabled );
+			}
+		} catch ( \Exception $e ) {
+			echo $e->getMessage();
+		}
+	}
 
-    /**
-     * Register the application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		//
+	}
 }
