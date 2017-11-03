7fe8a27b011721b2f5f7f4e5d4532dc0874b7aef
Artem Ivanko <ivankoartem@gmail.com>
2017-11-03 16:08:56 +0200

    Добавление новых настроек

---
+++ config/Billing.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Artem
+ * Date: 16.10.2017
+ * Time: 23:59
+ */
+
+
+return [
+	'Client' => [
+		'Role' => [
+			'Default'              => 'client',
+			'SuffixAutoCreateRole' => 'billing_',
+		],
+	]
+
+];
---
+++ config/TeamSpeakSnapshotsVirtualServers.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Artem
+ * Date: 22.10.2017
+ * Time: 12:42
+ */
+
+
+return [
+	'KeepDays'                              => 7,
+	'CreateSnapshotsDisabledVirtualServers' => false,
+];
